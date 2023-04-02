deafe6792666dc60dac381b4380fcda17ecc21e7
tmp <tmp@tmp>
2023-04-02 20:38:21 +0600

    Refactor styles

--- src/App.js
+++ src/App.js
@@ -26,16 +26,7 @@ function App() {
   });
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        background: "#ccc",
-      }}
-    >
+    <div className="App">
       <div>request: {fetchedData.join(", ")}</div>
       <Select
         options={options}
